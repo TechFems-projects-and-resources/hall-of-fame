b824f3d78f2c9cd765c360bb44e3de36b7f81a3a
artnerdnet <artnerdnet@gmail.com>
2022-05-01 12:50:10 +0200

    prettier settings

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,24 +2,19 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "airbnb",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended", "airbnb", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["react"],
   rules: {
     "react/react-in-jsx-scope": "off",
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-  },
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }]
+  }
 };
